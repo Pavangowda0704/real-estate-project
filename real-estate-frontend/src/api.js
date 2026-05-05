ad64a8fbecc21dd8f206344b20f17af5a3cf62f5
Shashankdurs <shashankdurs77@gamil.com>
2026-05-05 13:07:58 +0530

    Use deployed API environment variable

--- real-estate-frontend/src/api.js
+++ real-estate-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://real-estate-backend-fg09.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
